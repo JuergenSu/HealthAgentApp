aa6ca454e366b6982401662244e6910a81082b12
Juergen Sussner <36164641+JuergenSu@users.noreply.github.com>
2026-08-25 17:17:20 +0200

    feat(android): define onboarding steps

---
+++ android/app/src/main/java/de/fitnesscoach/ui/onboarding/OnboardingStep.java
@@ -0,0 +1,15 @@
+package de.fitnesscoach.ui.onboarding;
+
+public enum OnboardingStep {
+    WELCOME,
+    COACH_BOUNDARIES,
+    FITNESS_LEVEL,
+    SPORT_PREFERENCES,
+    TRAINING_AVAILABILITY,
+    PRIMARY_GOAL,
+    HEALTH_CONNECT,
+    INITIAL_SYNC,
+    BASELINE_STATUS,
+    INITIAL_PLAN,
+    COMPLETE
+}
