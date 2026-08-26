7e8fd11d6e54e6f70f699e489b97332b2b082edc
Juergen Sussner <36164641+JuergenSu@users.noreply.github.com>
2026-08-26 19:59:13 +0200

    Add coach chat message model (#32)

---
+++ android/app/src/main/java/de/fitnesscoach/ui/coach/CoachMessage.java
@@ -0,0 +1,2 @@
+package de.fitnesscoach.ui.coach;
+public final class CoachMessage{public final boolean user;public final String text;public CoachMessage(boolean user,String text){this.user=user;this.text=text;}}
